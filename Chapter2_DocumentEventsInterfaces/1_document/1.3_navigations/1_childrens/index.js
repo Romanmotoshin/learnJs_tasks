20d93f7a65258b6aa50087c1b1f965ee6e21ef72
Roman Motoshin <roman.motoshin@yandex.ru>
2023-03-08 17:16:35 +0300

    document in chapter 2 done

---
+++ Chapter2_DocumentEventsInterfaces/1_document/1.3_navigations/1_childrens/index.js
@@ -0,0 +1,5 @@
+document.body.firstElementChild // div
+
+document.body.lastElementChild // ul
+
+document.body.lastElementChild.lastElementChild // последний li
---
+++ Chapter2_DocumentEventsInterfaces/1_document/1.4_search/1_search/index.js
@@ -0,0 +1,11 @@
+const table = document.getElementById("age-table")
+
+const labels = table.querySelectorAll("label")
+
+const td = table.querySelector('td')
+
+const form = document.querySelector('form[name="search"]')
+
+const firstInput = form.querySelector('input')
+
+const lastInput = form.querySelectorAll('input')[inputs.length - 1]
